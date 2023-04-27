baa91dfdd6534eb3fe8f75bca7c67f46262101d1
melnik <1652169264@qq.com>
2023-04-27 14:11:41 +0800

    complete lab： threads

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -14,6 +14,7 @@ struct entry {
   struct entry *next;
 };
 struct entry *table[NBUCKET];
+pthread_mutex_t locks[NBUCKET];    
 int keys[NKEYS];
 int nthread = 1;
 
@@ -43,6 +44,7 @@ void put(int key, int value)
 
   // is the key already present?
   struct entry *e = 0;
+  pthread_mutex_lock(&locks[i]); 
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
@@ -54,6 +56,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&locks[i]); 
 
 }
 
@@ -105,6 +108,9 @@ main(int argc, char *argv[])
   void *value;
   double t1, t0;
 
+  for (int i = 0;i < NBUCKET; i++) {
+    pthread_mutex_init(&locks[i], NULL); 
+  }
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
